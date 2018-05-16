bf9a83a5d1f9b03e10dbe5b24398dd289291a8e6
James <jdkelly1985@gmail.com>
2018-05-16 08:32:59 +0000

    initial_base_template_setup

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,28 @@
+from flask import render_template
 from app import app
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World!'
+    return render_template('index.html', title='Home')
+
+
+@app.route('/linka')
+def linka():
+    return '<h1>Link A</h1>'
+
+
+@app.route('/linkb')
+def linkb():
+    return '<h1>Link B</h1>'
+
+
+@app.route('/login')
+def login():
+    return '<h1>Login</h1>'
+
+
+@app.route('/logout')
+def logout():
+    return '<h1>Logout</h1>'
